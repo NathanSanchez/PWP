53e2642322f468c7650b302a18f7bcaef8335563
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-31 10:22:03 -0600

    changed directory name, corrected .gitignore. css

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8"/>
 		<!-- add CSS file -->
 		<link href="../css/stylesheet.css" rel="stylesheet" type="text/css">
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<title>NATHANS CoOOoOOL WEBSITE</title>
 	</head>
 	<body>
@@ -12,7 +13,7 @@
 		<h1>The Purpose</h1>
 
 
-		<p>of this bad boy is to get me a job.</p>
+		<p>of this bad boy is to get me a job. Working in web development</p>
 
 		<h1>the attend audience</h1>
 
@@ -29,7 +30,11 @@
 		<h1>Use Case</h1>
 
 		<p>Hank comes to my website at my request on friday the 14th 2020 in tokyo using his gaming pc to test bugs on my website</p>
-			<h1></h1>
+
+			<h1>brief plan for this website</h1>
+
+			<p>my plan is to really get this site looking good right now im thinking about grey or green (possibly both)
+			i think it would be cool/kinda funny to show some of my code in the site(also having a github link). have links or qr codes to other websites/apps more info about me like what ive done up to this point, i want it to be someething that i can put on my resume.</p>
 
 		</section>
 
---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	</head>
+	<body>
+
+		<img src="Desktop HD.png" alt="" style="width: 128px;height:128px;">
+
+		<img src="Mobile Portrait.png" alt="" style="width: 128px;height:128px;">
+
+
+
+
+	</body>
+
+
+
+
+
+
+
+</html>
