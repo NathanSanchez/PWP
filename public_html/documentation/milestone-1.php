8d936b2f088befc58330a961d3dee1f152ba290a
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-14 17:24:40 -0700

    added that sexy header

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<title>NATHANS CoOOoOOL WEBSITE</title>
 	</head>
-
+<header>Nathan this is your header</header>
 	<body>
 		<section>
 
