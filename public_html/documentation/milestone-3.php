bbdafb1564f647141e548929eb7f05d7ebfb7ee6
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-23 00:14:30 -0700

    learning more about bootstrap

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -7,6 +7,8 @@
 
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
+
+
 		<!-- Bootstrap Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
 
@@ -34,24 +36,48 @@
 		<!-- NavBar -->
 		<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
 			<div class="container">
-			<div class="navbar-header">
-			</div><!-- end container -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-
+					collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+					</button>
+
+
+
+						<a href="" class="navbar-brand">Nathan Andrew Sanchez</a>
+					</div><!-- end of nav bar header -->
+					<div class="collapse navbar-collapse" id="navbar-collapse">
+						<ul class="nav navbar-nav">
+							<li><a href="#contactme">ContactMe</a>
+							<li><a href="#downloadmyresume">Download My Resume</a>
+						</ul>
+					</div>
+				</div><!-- end container -->
 		</nav><!-- end of the nav bar -->
 
+		<!--jumbotron-->
 
-		<div class="title">
-			<h1>Nathan Andrew Sanchez</h1>
+		<div class="jumbotron">
+			<div class="container text-center" >
+				<h1>Nathan Andrew Sanchez</h1>
+				<p>one cool dude</p> <!-- this defiantly isn't making it to the final draft =^. _ .^= -->
+				<div class="btn-group">
+					<a href="" class="btn-text">.contact info</a>
+					<a href="" class="btn-text">.resume</a>
+					<a href="" class="btn-text">.more info</a>
+				</div>
+			</div> <!-- this is where the container ends -->
+		</div><!-- the end of jumbotron-->
+
+
+
+		<div class="container">
+
 		</div>
-
-		<h2>Contact</h2>
-		<ul>
-			<li>nathanandrewsanchez@gmail.com (505)922-5975 </li>
-		</ul>
-		<h2>Experience</h2>
-		<p>Refuse to leave cardboard box. Shove bum in owner's face like camera lens poop on grasses pooping rainbow while flying in a toasted bread costume in space slap owner's face at 5am until human fills food dish and plan steps for world domination but purrrrrr destroy couch as revenge. If it fits, i sits kitty scratches couch bad kitty jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn and fights with dog cat chases laser then plays in grass finds tiny spot in cupboard and sleeps all day jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water. Sit by the fire eat grass, throw it back up purr while eating. Paw at beetle and eat it before it gets away cat slap dog in face meowzer! but refuse to leave cardboard box. Knock dish off table head butt cant eat out of my own dish curl up and sleep on the freshly laundered towels cats go for world domination and rub whiskers on bare skin act innocent. Meowing non stop for food plan steps for world domination rub whiskers on bare skin act innocent under the bed hide at bottom of staircase to trip human mrow. Put butt in owner's face stick butt in face lick arm hair so climb leg hide at bottom of staircase to trip human, eat and than sleep on your face, yet play time. Toy mouse squeak roll over groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!. Has closed eyes but still sees you go into a room to decide you didn't want to be in there anyway so paw at beetle and eat it before it gets away yet unwrap toilet paper flop over. I am the best rub face on everything ignore the squirrels, you'll never catch them anyway the dog smells bad meowwww, scratch the furniture. Destroy the blinds pooping rainbow while flying in a toasted bread costume in space. Cat is love, cat is life. I like big cats and i can not lie lick the plastic bag so scratch the box. This human feeds me, i should be a god fall asleep on the washing machine and sometimes switches in french and say "miaou" just because well why not or hunt anything that moves, but when in doubt, wash intrigued by the shower hide at bottom of staircase to trip human. Purrrrrr paw at your fat belly climb a tree, wait for a fireman jump to fireman then scratch his face for kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff stare at the wall, play with food and get confused by dust or spot something, big eyes, big eyes, crouch, shake butt, prepare to pounce.
-
-			Sweet beast human give me attention meow or russian blue. You call this cat food? use lap as chair, or shove bum in owner's face like camera lens damn that dog . Eat owner's food purr while eating, climb a tree, wait for a fireman jump to fireman then scratch his face, licks your face. Destroy the blinds. Fall asleep on the washing machine have my breakfast spaghetti yarn. Paw at your fat belly sleep on dog bed, force dog to sleep on floor. Asdflkjaertvlkjasntvkjn (sits on keyboard). Eat grass, throw it back up put toy mouse in food bowl run out of litter box at full speed so shake treat bag slap owner's face at 5am until human fills food dish. Toy mouse squeak roll over run outside as soon as door open lick the other cats or shake treat bag meow for food, then when human fills food dish, take a few bites of food and continue meowing yet eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap. Drink water out of the faucet. Scratch the furniture refuse to drink water except out of someone's glass. Hide when guests come over jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed. Chase imaginary bugs sit and stare who's the baby, for friends are not food, for run in circles. Mew chew iPad power cord, put toy mouse in food bowl run out of litter box at full speed for knock over christmas tree, knock over christmas tree.
-		</p>
+		</div><!-- end of container -->
 
 
 
