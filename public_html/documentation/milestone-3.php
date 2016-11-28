65a425e8e0da4766cc9a0ddeb2ad8f4fc74079ed
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-27 20:15:16 -0700

    cant get any of my fonts to work:/

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -69,7 +69,7 @@
 
 				<div class="container-fluid bg-1 text-center" >
 					<h3 class="margin">Nathan Sanchez</h3>
-					<img class="img-circle" src="tempphoto.jpg" alt="breadhead" height="200" width="200">
+					<img class="img-circle" src="tempphoto.jpg" alt="breadhead" height="200" width="200"><!-- temp photo until proper head shot -->
 					<h3>Web Developer</h3>
 
 					<div class="btn-group">
@@ -88,11 +88,16 @@
 			</div>
 		<div class="container text-center">
 			<p class="text-center">
-				My Name is Nathan Andrew Sanchez, ive been attending
-
-
-
-
+				My Name is Nathan Andrew Sanchez, I was born and raised in Albuquerque New Mexico.
+				Computers and video games have always been a huge part of my life from playings thousands of hours of video games
+				to building my own gaming pc. Computers and any kind of new tech excites me. The idea of being on the cutting edge of a new technologies is
+				really cool to me. One of my other grate passions in life is cars/motorcycles riding a motorcycle there's really nothing quite like it having the air
+				rushing around you as your shifting through the gears is a amazing feeling what i imagine is the closest thing to flying. Cars on the other hand not only do you get that amazing feeling of being put in the back of your seat
+				as you hit the gas, there's a huge style thing cars aren't just about speed there about style to being able to add you own style to a car is
+				what makes the car yours. About my background i was raised in a family of 5 as the youngest of 3, during high school i was homeschooled after high school
+				i went to deep dive coding bootcamp, a 40+ hour a week full stack web development class where i learned HTML5, JavaScript, Angular 2, MySQL, PHP and PHPUnit, CSS3, Bootstrap, GitHub, jQuery.
+				But i continue to learn and keep getting better and better. im hoping to be starting my career as a Web Developer in 2017 Coding is alot of fun but also has some big challengs
+				which i anticipate and am ready for.
 			</p>
 			</div>
 
