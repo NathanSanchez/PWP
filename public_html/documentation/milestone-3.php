d24e74d31d16f0f21d845af4e801a33743e254be
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-27 21:21:38 -0700

    cant get any of my fonts to work:/

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -53,10 +53,6 @@
 						<a href="" class="navbar-brand">Nathan Andrew Sanchez</a>
 					</div><!-- end of nav bar header -->
 					<div class="collapse navbar-collapse" id="navbar-collapse">
-						<ul class="nav navbar-nav">
-							<li><a href="#contactme">ContactMe</a>
-							<li><a href="#downloadmyresume">Download My Resume</a>
-						</ul>
 					</div>
 				</div><!-- end container -->
 		</nav><!-- end of the nav bar -->
@@ -68,17 +64,8 @@
 				<h1>Nathan Andrew Sanchez</h1>
 
 				<div class="container-fluid bg-1 text-center" >
-					<h3 class="margin">Nathan Sanchez</h3>
-					<img class="img-circle" src="tempphoto.jpg" alt="breadhead" height="200" width="200"><!-- temp photo until proper head shot -->
+					<img class="img-circle" src="tempphoto.jpg" alt="breadhead" height="300" width="300"><!-- temp photo until proper head shot -->
 					<h3>Web Developer</h3>
-
-					<div class="btn-group">
-						<a href="" class="btn-text">contact info</a>
-						<a href="" class="btn-text">resume</a>
-						<a href="" class="btn-text">info</a>
-					</div>
-
-
 			</div> <!-- this is where the container ends -->
 		</div><!-- the end of jumbotron-->
 
@@ -98,8 +85,52 @@
 				i went to deep dive coding bootcamp, a 40+ hour a week full stack web development class where i learned HTML5, JavaScript, Angular 2, MySQL, PHP and PHPUnit, CSS3, Bootstrap, GitHub, jQuery.
 				But i continue to learn and keep getting better and better. im hoping to be starting my career as a Web Developer in 2017 Coding is alot of fun but also has some big challengs
 				which i anticipate and am ready for.
-			</p>
+			</p><!-- temp writing for now -->
 			</div>
+
+			<div class="container bg-1 text-center">
+				<div class="container">
+					<h1 class="text-center">Experience</h1>
+				</div>
+				<div class="container text-center">
+					<p class="text-center">
+						For my Experience as a employee ive Had two jobs that ive done in the past. One of the jobs was working at dominos as a pizza maker / taker of phones.
+						it was at this job that i first leared about working as it was my first job that i got shortly after turing 16, working there was harder the expected but it
+						taught me alot of lessons such as working with a team and working under a suprising amount of stress. My seconed job that i got was working at hinkle family fun center
+						which is a family fun center so it has arcade games, lazertag, paintball, go karts, and all kinds of other fun stuff. I started there as a tranee then moved up to fun expert about
+						a year afteri started working there i got promoted to Team Leader where my job was to lead the Team in day to day task such as opening up, closing and dealing with
+						 frustrated customers. It was at this job where i leared the vaule of good customer service and how not only be on a team but also lead it.
+						for my Experience as a Web Dev as i mentioned above i went to deep dive coding bootcamp which is a full stack web development class where i learned
+						HTML5, JavaScript, Angular 2, MySQL, PHP and PHPUnit, CSS3, Bootstrap, GitHub, jQuery. In this class we get set in to groups where we begain development of a
+						web app of our choice called capstone. My capstone is called Cannaduceus its main perpose is to help medical marijuana patients find dispensarys near them and to help find the
+						right strain of marijuana for them. its actually pretty cool theres a lot of code that goes in to it such as a little bit of html5 a whole lot of php and of coures
+
+					</p><!-- temp writing for now -->
+				</div>
+
+
+
+
+
+
+
+
+				<nav class="navbar navbar-inverse navbar-fixed-bottom">
+				<div class="container">
+					<div class="navbar-header">
+						<a class="navbar-brand">Nathan Andrew Sanchez</a>
+					</div>
+					<div>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a>nathanandrewsanchez@gmail.com</a></li>
+							<li><a>(505)922-5975</a></li>
+							<li><a href="https://github.com/NathanSanchez">Github</a>
+							<li><a href="https://twitter.com/_NathanSanchez_">Twitter</a>
+							<li><a href="https://www.linkedin.com/profile/preview?locale=en_US&trk=prof-0-sb-preview-primary-button">Linkedin</a>
+							<li><a href="">Resume</a>
+						</ul>
+					</div>
+				</div>
 
 
 	</body>
