84c4ad890f31dd611bacdaf858a7f326de76e5ca
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-29 11:12:34 -0700

    please please be good

---
+++ public_html/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+		<link type="text/css" href="../css/stylesheet.css" rel="stylesheet" />
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!--fonts up in here -->
+		<link href="https://fonts.googleapis.com/css?family=Yrsa" rel="stylesheet">
+
+		<title>Nathan Andrew Sanchez</title>
+	</head>
+
+	<body>
+		<!-- NavBar -->
+		<nav class="navbar navbar-inverse navbar-fixed-top" id="my-navbar">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-
+					collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+
+
+
+					<a href="" class="navbar-brand">Nathan Andrew Sanchez</a>
+				</div><!-- end of nav bar header -->
+				<div class="collapse navbar-collapse" id="navbar-collapse">
+				</div>
+			</div><!-- end container -->
+		</nav><!-- end of the nav bar -->
+
+		<!--jumbotron-->
+
+		<div class="jumbotron">
+			<div class="container text-center" >
+				<h1>Nathan Andrew Sanchez</h1>
+
+				<div class="container-fluid bg-1 text-center" >
+					<img class="img-circle" src="tempphoto.jpg" alt="breadhead" height="300" width="300"><!-- temp photo until proper head shot -->
+					<h3>Web Developer</h3>
+				</div> <!-- this is where the container ends -->
+			</div><!-- the end of jumbotron-->
+
+			<div class="container bg-1 text-center">
+				<div class="container">
+					<h1 class="text-center">About Me</h1>
+				</div>
+				<div class="container text-center">
+					<p class="text-center">
+						My Name is Nathan Andrew Sanchez, I was born and raised in Albuquerque New Mexico.
+						Computers and video games have always been a huge part of my life from playings thousands of hours of video games
+						to building my own gaming pc. Computers and any kind of new tech excites me. The idea of being on the cutting edge of a new technologies is
+						really cool to me. One of my other grate passions in life is cars/motorcycles riding a motorcycle there's really nothing quite like it having the air
+						rushing around you as your shifting through the gears is a amazing feeling what i imagine is the closest thing to flying. Cars on the other hand not only do you get that amazing feeling of being put in the back of your seat
+						as you hit the gas, there's a huge style thing cars aren't just about speed there about style to being able to add you own style to a car is
+						what makes the car yours. About my background i was raised in a family of 5 as the youngest of 3, during high school i was homeschooled after high school
+						i went to deep dive coding bootcamp, a 40+ hour a week full stack web development class where i learned HTML5, JavaScript, Angular 2, MySQL, PHP and PHPUnit, CSS3, Bootstrap, GitHub, jQuery.
+						But i continue to learn and keep getting better and better. im hoping to be starting my career as a Web Developer in 2017 Coding is alot of fun but also has some big challengs
+						which i anticipate and am ready for.
+					</p><!-- temp writing for now -->
+				</div>
+
+				<div class="container bg-1 text-center">
+					<div class="container">
+						<h1 class="text-center">Experience</h1>
+					</div>
+					<div class="container text-center">
+						<p class="text-center">
+							For my Experience as a employee ive Had two jobs that ive done in the past. One of the jobs was working at dominos as a pizza maker / taker of phones.
+							it was at this job that i first leared about working as it was my first job that i got shortly after turing 16, working there was harder the expected but it
+							taught me alot of lessons such as working with a team and working under a sunrising amount of stress. My second job that i got was working at hinkle family fun center
+							which is a family fun center so it has arcade games, lazertag, paintball, go karts, and all kinds of other fun stuff. I started there as a trainee then moved up to fun expert about
+							a year afteri started working there i got promoted to Team Leader where my job was to lead the Team in day to day task such as opening up, closing and dealing with
+							frustrated customers. It was at this job where i learned the value of good customer service and how not only be on a team but also lead it.
+							for my Experience as a Web Dev as i mentioned above i went to deep dive coding bootcamp which is a full stack web development class where i learned
+							HTML5, JavaScript, Angular 2, MySQL, PHP and PHPUnit, CSS3, Bootstrap, GitHub, jQuery. In this class we get set in to groups where we began development of a
+							web app of our choice called capstone. My capstone is called Cannaduceus its main purpose is to help medical marijuana patients find dispensary's near them and to help find the
+							right strain of marijuana for them. its actually pretty cool theres a lot of code that goes in to it such as a little bit of html5 a whole lot of php and of course Css and Anglur abd a couple more.
+							Last but not least this here website!
+						</p><!-- temp writing for now -->
+					</div>
+
+
+
+
+
+
+
+
+					<nav class="navbar navbar-inverse navbar-fixed-bottom">
+						<div class="container">
+							<div class="navbar-header">
+								<a class="navbar-brand">Nathan Andrew Sanchez</a>
+							</div>
+							<div>
+								<ul class="nav navbar-nav navbar-right">
+									<li><a>nathanandrewsanchez@gmail.com</a></li>
+									<li><a>(505)922-5975</a></li>
+									<li><a href="https://github.com/NathanSanchez">Github</a>
+									<li><a href="https://twitter.com/_NathanSanchez_">Twitter</a>
+									<li><a href="https://www.linkedin.com/profile/preview?locale=en_US&trk=prof-0-sb-preview-primary-button">Linkedin</a>
+									<li><a href="">Resume</a>
+								</ul>
+							</div>
+						</div>
+
+
+	</body>
+
+
+
+
+
+</html>
