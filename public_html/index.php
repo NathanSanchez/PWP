076f8ffd89fffc522e944f9ebe8a5c15fe231325
Nathan Sanchez <nsanchez121@cnm.edu>
2016-11-30 16:55:28 -0700

    too sleepy to keep going

--- public_html/index.php
+++ public_html/index.php
@@ -7,20 +7,16 @@
 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link type="text/css" href="../css/stylesheet.css" rel="stylesheet" />
-
-		<!--fonts up in here -->
-		<link href="https://fonts.googleapis.com/css?family=Yrsa" rel="stylesheet">
-
-
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!--fonts up in here -->
+		<link href="https://fonts.googleapis.com/css?family=Yrsa" rel="stylesheet">
+
+		<link type="text/css" href="../css/stylesheet.css" rel="stylesheet" />
 
 		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -34,9 +30,6 @@
 
 		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-		<!-- jQuery -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
 		<!-- jQuery Form, Additional Methods, Validate -->
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
@@ -79,31 +72,26 @@
 		<div class="jumbotron">
 			<div class="container text-center" >
 				<h1>Nathan Andrew Sanchez</h1>
-
-				<div class="container-fluid bg-1 text-center" >
-					<img class="img-circle" src="tempphoto.jpg" alt="breadhead"><!-- temp photo until proper head shot -->
-					<h3>Web Developer</h3>
-				</div> <!-- this is where the container ends -->
+				<img class="img-circle" src="tempphoto.jpg" alt="breadhead"><!-- temp photo until proper head shot -->
+				<h3>Web Developer</h3>
 			</div><!-- the end of jumbotron-->
+		</div>
 
-			<div class="container bg-1 text-center">
-				<div class="container">
-					<h1 class="text-center">About Me</h1>
-				</div>
-				<div class="container text-center">
-					<p class="text-center">
-						My Name is Nathan Andrew Sanchez, I was born and raised in Albuquerque New Mexico.
-						Computers and video games have always been a huge part of my life from playings thousands of hours of video games
-						to building my own gaming pc. Computers and any kind of new tech excites me. The idea of being on the cutting edge of a new technologies is
-						really cool to me. One of my other grate passions in life is cars/motorcycles riding a motorcycle there's really nothing quite like it having the air
-						rushing around you as your shifting through the gears is a amazing feeling what i imagine is the closest thing to flying. Cars on the other hand not only do you get that amazing feeling of being put in the back of your seat
-						as you hit the gas, there's a huge style thing cars aren't just about speed there about style to being able to add you own style to a car is
-						what makes the car yours. About my background i was raised in a family of 5 as the youngest of 3, during high school i was homeschooled after high school
-						i went to deep dive coding bootcamp, a 40+ hour a week full stack web development class where i learned HTML5, JavaScript, Angular 2, MySQL, PHP and PHPUnit, CSS3, Bootstrap, GitHub, jQuery.
-						But i continue to learn and keep getting better and better. im hoping to be starting my career as a Web Developer in 2017 Coding is alot of fun but also has some big challengs
-						which i anticipate and am ready for.
-					</p><!-- temp writing for now -->
-				</div>
+		<div class="container bg-1 text-center">
+			<h1 class="text-center">About Me</h1>
+			<p class="text-center">
+				My Name is Nathan Andrew Sanchez, I was born and raised in Albuquerque New Mexico.
+				Computers and video games have always been a huge part of my life from playings thousands of hours of video games
+				to building my own gaming pc. Computers and any kind of new tech excites me. The idea of being on the cutting edge of a new technologies is
+				really cool to me. One of my other grate passions in life is cars/motorcycles riding a motorcycle there's really nothing quite like it having the air
+				rushing around you as your shifting through the gears is a amazing feeling what i imagine is the closest thing to flying. Cars on the other hand not only do you get that amazing feeling of being put in the back of your seat
+				as you hit the gas, there's a huge style thing cars aren't just about speed there about style to being able to add you own style to a car is
+				what makes the car yours. About my background i was raised in a family of 5 as the youngest of 3, during high school i was homeschooled after high school
+				i went to deep dive coding bootcamp, a 40+ hour a week full stack web development class where i learned HTML5, JavaScript, Angular 2, MySQL, PHP and PHPUnit, CSS3, Bootstrap, GitHub, jQuery.
+				But i continue to learn and keep getting better and better. im hoping to be starting my career as a Web Developer in 2017 Coding is alot of fun but also has some big challengs
+				which i anticipate and am ready for.
+			</p>
+
 
 				<div class="container bg-1 text-center">
 					<div class="container">
